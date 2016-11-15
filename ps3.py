159bb8a3419360d23e48822b706a90b4167c5918
2016jtm2086 <m.s.c.thakur@gmail.com>
2016-11-15 10:42:07 +0530

    updated

--- ps3.py
+++ ps3.py
@@ -1,58 +1,69 @@
+###### this is the third .py file ###########
+state={"UTTARAKHAND":'000',"GOA":'001',"NEW DELHI":'010',"PUNJAB":'011'}
+city={"ROORKEE":'111',"NEW DELHI":'110',"PANAJI":'101',"AMRITSAR":'100'}
+district={"IIT ROORKEE":'000',"NIT GOA":'001',"IIT DELHI":'010',"NIT JALANDHAR":'011'}
+print "1.add 2.modify 3.delete 4.query"
+no=input("enter the operation you want to perform")
+if no==1:
+	s=raw_input("enter the state name you want to add")
+	m=raw_input("enter the code name you want to add for state")
+	state.update({s:m})
+	c=raw_input("enter the city name you want to add")
+	n=raw_input("enter the code name you want to add for city")
+	city.update({c:n})
+	d=raw_input("enter the district name you want to add")
+	q=raw_input("enter the code name you want to add for district")
+	district.update({d:q})
+	
 
-state={"UP":'000',"Raj":'001',"MP":'010',"AP":'011'}
-city={"lucknow":'111',"jaipur":'110',"bhopal":'101',"Amravati":'100'}
-district={"IET lucknow":'000',"NIT jaipur":'001',"mnnit":'010',"NIT warangal":'011'}
-print "1.add  2.delete 3.modify"
-a=input("Changes you want")
-if a==1:
-    s=raw_input("state name")
-    m=raw_input("code name you for state")
-    state.update({s:m})
-    c=raw_input("city name")
-    n=raw_input("code name for city")
-    city.update({c:n})
-    d=raw_input("district name")
-    q=raw_input("code name for district")
-    district.update({d:q})
-   
-if a==2:   
-    b=raw_input("state to be deleted\n")
-    if b in state.keys():   
-        del state[b]
-    else :
-        print "invalid key"
-    b=raw_input("city to be deleted\n")
-    if b in city.keys():   
-        del city[b]
-    else :
-        print "invalid key"
-    b=raw_input("district to be deleted\n")
-    if b in district.keys():   
-        del district[b]
-    else :
-        print "invalid key"
-if a==3:
-    t1=raw_input("state code to be modified\n")
-    t11=raw_input("enter the new code\n")
-    if t1 in state.keys():
-        state.update({t1:t11})
-    else :
-        print 'invalid state'
-    t2=raw_input("district code  to be modified\n")
-    t21=raw_input("enter the new code\n")
-    if t2 in district.keys():
-        district.update({t2:t21})
-    else :
-        print 'invalid district'
-    t3=raw_input("city code to be modified\n")
-    t31=raw_input("enter the new code\n")
-    if t3 in city.keys():
-        city.update({t3:t31})
-    else :
-        print 'invalid city'
+if no==2:
+	t1=raw_input("enter the state whose code you want to modify\n")
+	t11=raw_input("enter the new code\n")
+	if t1 in state.keys():
+		state.update({t1:t11})
+	else :
+		print 'not a valid state'
+	t2=raw_input("enter the district whose code you want to modify\n")
+	t21=raw_input("enter the new code\n")
+	if t2 in district.keys():
+		district.update({t2:t21})
+	else :
+		print 'not a valid district'
+	t3=raw_input("enter the city whose code you want to modify\n")
+	t31=raw_input("enter the new code\n")
+	if t3 in city.keys():
+		city.update({t3:t31})
+	else :
+		print 'not a valid city'
+
+if no==3:	
+	st=raw_input("enter the state want to delete\n")
+	if st in state.keys():	
+		del state[b]
+	else :
+		print "not a valid key"
+	ci=raw_input("enter the city want to delete\n")
+	if ci in city.keys():	
+		del city[b]
+	else :
+		print "not a valid key" 
+	dt=raw_input("enter the district want to delete\n")
+	if dt in district.keys():	
+		del district[b]
+	else :
+		print "not a valid key"
 else :
-    l1=raw_input("enter the customer name\n")
-    l2=raw_input("enter the customer district\n")
-    l3=raw_input("enter the customer city\n")
-    l4=raw_input("enter the customer state\n")
-   
+	l1=raw_input("enter the customer name\n")
+	l2=raw_input("enter the customer district\n")
+	l3=raw_input("enter the customer city\n")
+	l4=raw_input("enter the customer state\n")
+	if l4 in state.keys():
+		c= state.get(l4)
+	if l2 in district.keys():
+		d= district.get(l2)
+	if l3 in city.keys():
+		f= city.get(l3)
+	print "Human Readable code"
+	str=" "
+	seq=(c,d,f)
+print "CC.NO=",str.join(seq)
